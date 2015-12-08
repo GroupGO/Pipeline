5b6456c4ed1871cc2b16027bb089047c3ea47f90
GroupGO <henry.ehlers@wur.nl>
2015-12-08 11:44:15 +0100

    2015_12_08

---
+++ __init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+"""
+Author: Henry Ehlers
+WUR_Number: 921013218060
+
+A script designed to ...
+    -input:
+    -output:
+
+In order to provide readable and understandable code, the right indentation margin has been
+increased from 79 to 99 characters, which remains in line with Python-Style-Recommendation (
+https://www.python.org/dev/peps/pep-0008/) .This allows for longer, more descriptive variable
+and function names, as well as more extensive doc-strings.
+"""
